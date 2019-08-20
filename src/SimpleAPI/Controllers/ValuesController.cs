db757616a4ddb3c9f8f6a4cfaf90b57fb382578f
Brijen Makwana <makwanabrijen@gmail.com>
2019-08-20 23:47:20 +0530

    fixed unit test

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "MS Dhoni";
+            return "Brijen Makwana";
         }
 
         // POST api/values
